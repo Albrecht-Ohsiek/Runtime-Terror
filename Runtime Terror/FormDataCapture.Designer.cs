8386271c4a341c5b226f881e38df47a351006a4f
Fenrir-23 <germangeneral2301@gmail.com>
2022-11-13 12:03:27 +0200

    Recreated DataCaptureUI
    Created DataCuptureGui rather than copying on from an external project

--- Runtime Terror/FormDataCapture.Designer.cs
+++ Runtime Terror/FormDataCapture.Designer.cs
@@ -28,20 +28,272 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelStudentID = new System.Windows.Forms.Label();
+            this.textBoxStudentID = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonStoreInfo = new System.Windows.Forms.Button();
+            this.comboBoxGender = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerDOB = new System.Windows.Forms.DateTimePicker();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxModule = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonDisplay = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelDOB = new System.Windows.Forms.Label();
+            this.labelPhoneNum = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelModule = new System.Windows.Forms.Label();
+            this.textBoxPhoneNum = new System.Windows.Forms.TextBox();
+            this.labelGender = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // labelStudentID
+            // 
+            this.labelStudentID.AutoSize = true;
+            this.labelStudentID.Location = new System.Drawing.Point(55, 26);
+            this.labelStudentID.Name = "labelStudentID";
+            this.labelStudentID.Size = new System.Drawing.Size(58, 13);
+            this.labelStudentID.TabIndex = 0;
+            this.labelStudentID.Text = "Student ID";
+            // 
+            // textBoxStudentID
+            // 
+            this.textBoxStudentID.Location = new System.Drawing.Point(220, 26);
+            this.textBoxStudentID.Name = "textBoxStudentID";
+            this.textBoxStudentID.Size = new System.Drawing.Size(253, 20);
+            this.textBoxStudentID.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(159, 265);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(611, 169);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // buttonStoreInfo
+            // 
+            this.buttonStoreInfo.Location = new System.Drawing.Point(16, 265);
+            this.buttonStoreInfo.Name = "buttonStoreInfo";
+            this.buttonStoreInfo.Size = new System.Drawing.Size(137, 29);
+            this.buttonStoreInfo.TabIndex = 3;
+            this.buttonStoreInfo.Text = "Store Information";
+            this.buttonStoreInfo.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxGender
+            // 
+            this.comboBoxGender.FormattingEnabled = true;
+            this.comboBoxGender.Location = new System.Drawing.Point(220, 160);
+            this.comboBoxGender.Name = "comboBoxGender";
+            this.comboBoxGender.Size = new System.Drawing.Size(172, 21);
+            this.comboBoxGender.TabIndex = 4;
+            // 
+            // dateTimePickerDOB
+            // 
+            this.dateTimePickerDOB.Location = new System.Drawing.Point(220, 108);
+            this.dateTimePickerDOB.Name = "dateTimePickerDOB";
+            this.dateTimePickerDOB.Size = new System.Drawing.Size(253, 20);
+            this.dateTimePickerDOB.TabIndex = 5;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(220, 52);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(253, 20);
+            this.textBoxName.TabIndex = 6;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(220, 78);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(253, 20);
+            this.textBoxLastName.TabIndex = 7;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(220, 187);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(253, 20);
+            this.textBoxEmail.TabIndex = 8;
+            // 
+            // textBoxModule
+            // 
+            this.textBoxModule.Location = new System.Drawing.Point(220, 213);
+            this.textBoxModule.Name = "textBoxModule";
+            this.textBoxModule.Size = new System.Drawing.Size(253, 20);
+            this.textBoxModule.TabIndex = 9;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(16, 300);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(137, 29);
+            this.buttonSearch.TabIndex = 10;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(16, 335);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(137, 29);
+            this.buttonUpdate.TabIndex = 11;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(16, 370);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(137, 29);
+            this.buttonDelete.TabIndex = 12;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonDisplay
+            // 
+            this.buttonDisplay.Location = new System.Drawing.Point(16, 405);
+            this.buttonDisplay.Name = "buttonDisplay";
+            this.buttonDisplay.Size = new System.Drawing.Size(137, 29);
+            this.buttonDisplay.TabIndex = 13;
+            this.buttonDisplay.Text = "Display";
+            this.buttonDisplay.UseVisualStyleBackColor = true;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(55, 55);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(75, 13);
+            this.labelName.TabIndex = 14;
+            this.labelName.Text = "Student Name";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(55, 85);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(89, 13);
+            this.labelLastName.TabIndex = 15;
+            this.labelLastName.Text = "Student Surname";
+            // 
+            // labelDOB
+            // 
+            this.labelDOB.AutoSize = true;
+            this.labelDOB.Location = new System.Drawing.Point(55, 115);
+            this.labelDOB.Name = "labelDOB";
+            this.labelDOB.Size = new System.Drawing.Size(66, 13);
+            this.labelDOB.TabIndex = 16;
+            this.labelDOB.Text = "Date of Birth";
+            // 
+            // labelPhoneNum
+            // 
+            this.labelPhoneNum.AutoSize = true;
+            this.labelPhoneNum.Location = new System.Drawing.Point(55, 142);
+            this.labelPhoneNum.Name = "labelPhoneNum";
+            this.labelPhoneNum.Size = new System.Drawing.Size(78, 13);
+            this.labelPhoneNum.TabIndex = 17;
+            this.labelPhoneNum.Text = "Phone Number";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(55, 194);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(73, 13);
+            this.labelEmail.TabIndex = 18;
+            this.labelEmail.Text = "Email Address";
+            // 
+            // labelModule
+            // 
+            this.labelModule.AutoSize = true;
+            this.labelModule.Location = new System.Drawing.Point(55, 220);
+            this.labelModule.Name = "labelModule";
+            this.labelModule.Size = new System.Drawing.Size(42, 13);
+            this.labelModule.TabIndex = 19;
+            this.labelModule.Text = "Module";
+            // 
+            // textBoxPhoneNum
+            // 
+            this.textBoxPhoneNum.Location = new System.Drawing.Point(220, 134);
+            this.textBoxPhoneNum.Name = "textBoxPhoneNum";
+            this.textBoxPhoneNum.Size = new System.Drawing.Size(253, 20);
+            this.textBoxPhoneNum.TabIndex = 20;
+            // 
+            // labelGender
+            // 
+            this.labelGender.AutoSize = true;
+            this.labelGender.Location = new System.Drawing.Point(55, 168);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(42, 13);
+            this.labelGender.TabIndex = 21;
+            this.labelGender.Text = "Gender";
             // 
             // FormDataCapture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(787, 444);
+            this.Controls.Add(this.labelGender);
+            this.Controls.Add(this.textBoxPhoneNum);
+            this.Controls.Add(this.labelModule);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelPhoneNum);
+            this.Controls.Add(this.labelDOB);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.buttonDisplay);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxModule);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.dateTimePickerDOB);
+            this.Controls.Add(this.comboBoxGender);
+            this.Controls.Add(this.buttonStoreInfo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxStudentID);
+            this.Controls.Add(this.labelStudentID);
             this.Name = "FormDataCapture";
             this.Text = "Form2DataCapture";
             this.Load += new System.EventHandler(this.FormDataCapture_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelStudentID;
+        private System.Windows.Forms.TextBox textBoxStudentID;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonStoreInfo;
+        private System.Windows.Forms.ComboBox comboBoxGender;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDOB;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxModule;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonDisplay;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelDOB;
+        private System.Windows.Forms.Label labelPhoneNum;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelModule;
+        private System.Windows.Forms.TextBox textBoxPhoneNum;
+        private System.Windows.Forms.Label labelGender;
     }
 }
